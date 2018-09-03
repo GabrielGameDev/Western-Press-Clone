a0683ec9f35fdaf53e0dcf161eb3fb893a125595
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2018-09-03 10:35:41 -0300

    Add files via upload

---
+++ LevelController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+public class Keys
+{
+	public Sprite keySprite;
+	public KeyCode key;
+}
+
+public class LevelController : MonoBehaviour {
+
+	public static LevelController instance;
+
+	public Keys[] keys;
+
+	public List<Keys> gameKeys;
+
+	public Image[] player1Images;
+	public Image[] player2Images;
+
+	public bool canPlay = false;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(StartingKeys());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator StartingKeys()
+	{
+		for (int i = 0; i < player1Images.Length; i++)
+		{
+			gameKeys.Add(keys[UnityEngine.Random.Range(0, keys.Length)]);
+			player1Images[i].sprite = gameKeys[i].keySprite;
+			player1Images[i].preserveAspect = true;
+			player1Images[i].enabled = true;
+			player2Images[i].sprite = gameKeys[i].keySprite;
+			player2Images[i].preserveAspect = true;
+			player2Images[i].enabled = true;
+			yield return new WaitForSeconds(0.25f);
+
+		}
+
+		canPlay = true;
+
+	}
+
+	public void NextKey(int playerIndex, int keyIndex)
+	{
+		if(playerIndex == 0)
+		{
+			player1Images[keyIndex].enabled = false;
+		}
+		else
+		{
+			player2Images[keyIndex].enabled = false;
+		}
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int playerIndex;
+
+	private int keyIndex = 8;
+	private bool canPlay = false;
+	// Use this for initialization
+	IEnumerator Start () {
+
+		while (LevelController.instance.canPlay == false)
+		{
+			yield return null;
+		}
+
+		canPlay = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(canPlay == false)
+		{
+			return;
+		}
+
+		if (Input.GetKeyDown(LevelController.instance.gameKeys[keyIndex].key))
+		{
+			KeyPress();
+		}
+
+	}
+
+	void KeyPress()
+	{
+		LevelController.instance.NextKey(playerIndex, keyIndex);
+		keyIndex--;
+
+		if (keyIndex < 0)
+			canPlay = false;
+	}
+}
